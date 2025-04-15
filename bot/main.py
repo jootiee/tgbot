3094624f3950701cc21815ac9e40420cd7637aac
jootiee <purrrtov@ya.ru>
2025-04-15 23:08:10 +0300

    feat: sending message to admin on bot startup and shutdown

--- bot/main.py
+++ bot/main.py
@@ -13,7 +13,8 @@ from handlers import register_user_handlers, create_commands
 
 
 async def main():
-    bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode='MarkdownV2'))
+    bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(
+        parse_mode='MarkdownV2'))
     dp = Dispatcher()
     router = Router()
     router.message.middleware(IsSubscribedMiddleware())
@@ -21,12 +22,10 @@ async def main():
     dp.include_router(router)
 
     await bot.set_my_commands(commands=create_commands())
-    # TODO:
-    # dp.startup.register(on_startup)
-    # dp.shutdown.register(on_shutdown)
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
 
     register_user_handlers(router)
-    
 
     await bot.delete_webhook(drop_pending_updates=True)
     try:
@@ -36,7 +35,7 @@ async def main():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, 
+    logging.basicConfig(level=logging.INFO,
                         format="%(asctime)s - [%(levelname)s] - [%(name)s] - "
                         "(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s")
     asyncio.run(main())
--- bot/utils/notify_admins.py
+++ bot/utils/notify_admins.py
@@ -1,18 +1,20 @@
 import logging
 from datetime import datetime as dt
 
-from aiogram import Dispatcher
+from aiogram import Bot
 
 from data.config import ADMIN_ID
 
 
-# async def on_startup(dp: Dispatcher):
-async def on_startup():
-    pass
-    # try:
-        # await dp.bot.send_message(ADMIN_ID, "Bot is running" + str(dt.now()))
-    # except Exception as err:
-        # logging.exception(err)
+async def on_startup(bot: Bot):
+    try:
+        await bot.send_message(ADMIN_ID, "Bot is running: " + str(dt.strftime(dt.now(), "%Y\\-%m\\-%d %H\\:%M\\:%S")))
+    except Exception as err:
+        logging.exception(err)
 
-async def on_shutdown():
-    pass
+
+async def on_shutdown(bot: Bot):
+    try:
+        await bot.send_message(ADMIN_ID, "Bot has stopped: " + str(dt.strftime(dt.now(), "%Y\\-%m\\-%d %H\\:%M\\:%S")))
+    except Exception as err:
+        logging.exception(err)
